dd3012601a67008a5de12e20689b05a9ac690257
Kauan <raidoka546@gmail.com>
2024-04-03 21:56:03 -0300

    Created function to interact with the menu, according to the item clicked it will show the correspondent content according to the html

--- main.js
+++ main.js
@@ -1,9 +1,37 @@
 document.addEventListener('DOMContentLoaded', function (event) {
     event.preventDefault();
-    const menuToggle = document.querySelector('.menu-toggle');
-    const navMenu = document.querySelector('.nav-menu');
+    function menuOnMobile(){
+        const menuToggle = document.querySelector('.menu-toggle');
+        const navMenu = document.querySelector('.nav-menu');
 
-    menuToggle.addEventListener('click', function() {
-        navMenu.classList.toggle('open');
-    });
+        menuToggle.addEventListener('click', function() {
+            navMenu.classList.toggle('open');
+        });
+    }
+
+    menuOnMobile();
+})
+
+document.addEventListener("DOMContentLoaded", function() {
+    
+    const navLinks = document.querySelectorAll(".nav-menu a");
+
+    navLinks.forEach(link => {
+        link.addEventListener("click", function(event) {
+        event.preventDefault();
+        
+        const href = this.getAttribute("href");
+        loadContent(href);
+        });
+  });
+
+    function loadContent(url) {
+        fetch(url)
+        .then(response => response.text())
+        .then(html => {
+            const content = document.getElementById("content");
+            content.innerHTML = html;
+        })
+        .catch(error => console.error("Erro ao carregar o conteúdo:", error));
+    };
 })
